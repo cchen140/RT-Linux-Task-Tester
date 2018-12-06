39034c86078659cf69f51e0735ab6676f262bd78
CY Chen <cchen140@illinois.edu>
2018-12-06 07:55:11 +0000

    Fxied a bug that initializes a 2d list with an incorrect way.

--- post_process.py
+++ post_process.py
@@ -13,20 +13,22 @@ if __name__ == '__main__':
     else:
         logFolderPath = DEFAULT_LOG_FOLDER_PATH
 
-    rawMeanCostListByNumOfTasks = [[]] * 11 # 0 is not used.
+    rawMeanCostListByNumOfTasks = [[] for i in xrange(11)] # 0 is not used.
     for i in xrange(1,9):
-        summaryFilePath = logFolderPath + "/" + SUB_LOG_FOLDER_NAME.format(i) + "/" + SUB_LOG_FOLDER_NAME
+        summaryFilePath = logFolderPath + "/" + SUB_LOG_FOLDER_NAME.format(i) + "/" + SUMMARY_FILE_NAME
         with open(summaryFilePath, "r") as lines:
             for line in lines:
-                numOfTasks = int(line.split(":")[0])
+                numOfTasks = int(line.split(":")[1])
                 thisMeanCost = int(line.split("mean:")[1].split(",")[0])
+                if thisMeanCost < 0:
+                    continue
                 rawMeanCostListByNumOfTasks[numOfTasks].append(thisMeanCost)
 
     outputFile = open(logFolderPath + "/" + OUTPUT_FILE_NAME, "w")
     meanMeanCostList = [0]*11 # 0 is unused.
     meanMeanCostStderrorList = [0]*11 # 0 is unused
     for i in xrange(1, 11):
-        meanMeanCostList[i] = sum(rawMeanCostListByNumOfTasks[i])/rawMeanCostListByNumOfTasks[i]
+        meanMeanCostList[i] = sum(rawMeanCostListByNumOfTasks[i])/len(rawMeanCostListByNumOfTasks[i])
         meanMeanCostStderrorList[i] = numpy.std(rawMeanCostListByNumOfTasks[i])/(len(rawMeanCostListByNumOfTasks[i])**0.5)
         outputString = "{}, {}\n".format(meanMeanCostList[i], meanMeanCostStderrorList[i])
         outputFile.write(outputString)
