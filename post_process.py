df1831b2fa6cd44eb30d3043956dab1432b92925
CY Chen <cchen140@illinois.edu>
2018-12-06 01:06:23 -0600

    Added a post process program for computing mean costs.

---
+++ post_process.py
@@ -0,0 +1,33 @@
+import sys
+import numpy
+
+
+DEFAULT_LOG_FOLDER_PATH = "log"
+SUB_LOG_FOLDER_NAME = "log{}"
+SUMMARY_FILE_NAME = "summary.txt"
+OUTPUT_FILE_NAME = "post_summary.txt"
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        logFolderPath = sys.argv[1]
+    else:
+        logFolderPath = DEFAULT_LOG_FOLDER_PATH
+
+    rawMeanCostListByNumOfTasks = [[]] * 11 # 0 is not used.
+    for i in xrange(1,9):
+        summaryFilePath = logFolderPath + "/" + SUB_LOG_FOLDER_NAME.format(i) + "/" + SUB_LOG_FOLDER_NAME
+        with open(summaryFilePath, "r") as lines:
+            for line in lines:
+                numOfTasks = int(line.split(":")[0])
+                thisMeanCost = int(line.split("mean:")[1].split(",")[0])
+                rawMeanCostListByNumOfTasks[numOfTasks].append(thisMeanCost)
+
+    outputFile = open(logFolderPath + "/" + OUTPUT_FILE_NAME, "w")
+    meanMeanCostList = [0]*11 # 0 is unused.
+    meanMeanCostStderrorList = [0]*11 # 0 is unused
+    for i in xrange(1, 11):
+        meanMeanCostList[i] = sum(rawMeanCostListByNumOfTasks[i])/rawMeanCostListByNumOfTasks[i]
+        meanMeanCostStderrorList[i] = numpy.std(rawMeanCostListByNumOfTasks[i])/(len(rawMeanCostListByNumOfTasks[i])**0.5)
+        outputString = "{}, {}\n".format(meanMeanCostList[i], meanMeanCostStderrorList[i])
+        outputFile.write(outputString)
+        print str(i) + ":" + outputString[:-1] # -1 is to remove "\n"
